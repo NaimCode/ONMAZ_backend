036b0692998d7511bc38bb2e23e753dd08bf9907
Mahamat Naim Abdelkerim <73735806+NaimCode@users.noreply.github.com>
2021-10-10 17:26:55 +0100

    trying to fix branch problem

--- app.js
+++ app.js
@@ -14,4 +14,5 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use("/blogs", blogsRoute);
+app.get("/", (req, res) => res.send("Server is running"));
 app.listen(port, () => console.log("Server is running"));
